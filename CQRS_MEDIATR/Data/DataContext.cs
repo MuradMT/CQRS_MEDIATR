b9dcb5759156f8d57222099b645afa55b1db5248
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-26 21:24:50 +0400

    Add project files.

---
+++ CQRS_MEDIATR/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+using CQRS_MEDIATR.Model;
+
+namespace CQRS_MEDIATR.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
---
+++ CQRS_MEDIATR/Model/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CQRS_MEDIATR.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+    }
+}
