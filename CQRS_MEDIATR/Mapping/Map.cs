70fbf930b1272108bfd8e40b647ac30de5e83265
Murad Mammadzade <mammadzade03@gmail.com>
2023-01-06 21:54:45 +0400

    Add mapping

---
+++ CQRS_MEDIATR/Mapping/Map.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CQRS_MEDIATR.Model;
+using CQRS_MEDIATR.Model.Dto;
+
+namespace CQRS_MEDIATR.Mapping
+{
+    public class Map:Profile
+    {
+        public Map() {
+            CreateMap<StudentCreateDto,Student>().ReverseMap();
+            CreateMap<StudentUpdateDto, Student>().ReverseMap();
+        }
+    }
+}
