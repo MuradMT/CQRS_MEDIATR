f9f80f1739a67fefe10dccff1b7cb9cfc3e28d06
Murad Mammadzade <mammadzade03@gmail.com>
2023-01-11 14:53:38 +0400

    do few modification

--- CQRS_MEDIATR/Model/ServiceResponse.cs
+++ CQRS_MEDIATR/Model/ServiceResponse.cs
@@ -2,7 +2,7 @@
 {
     public class ServiceResponse
     {
-        public object? Data { get; set; };
+        public object? Data { get; set; }
         public bool Success { get; set; } = true;
         public string Message { get; set; } = "there is no problem,everything is ok";
     }
