72ec003a3da5f200226f667505b9352832d982fd
Murad Mammadzade <mammadzade03@gmail.com>
2023-01-11 14:48:16 +0400

    change fat controller to thin controller.Doing this operations i used mediator and cqrs design patterns

--- CQRS_MEDIATR/Controllers/StudentsController.cs
+++ CQRS_MEDIATR/Controllers/StudentsController.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using Azure;
+using CQRS_MEDIATR.Commands;
 using CQRS_MEDIATR.Mapping;
 using CQRS_MEDIATR.Model;
 using CQRS_MEDIATR.Model.Dto;
+using CQRS_MEDIATR.Queries;
 using CQRS_MEDIATR.Services.Abstract.BaseService;
 using CQRS_MEDIATR.Services.Abstract.StudentService;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -15,156 +18,62 @@ namespace CQRS_MEDIATR.Controllers
     [ApiController]
     public class StudentsController : ControllerBase
     {
-        //Tomorrow i will start to use cqrs architectural and mediatr  design patterns
-        private IStudentService _service;
-        private IMapper _mapper;
-        private ILogger<Student> _logger;
-        public StudentsController(IStudentService service, IMapper mapper, ILogger<Student> logger)
+        private IMediator _mediator;
+        public StudentsController(IMediator mediator)
         {
-            _service = service;
-            _mapper = mapper;
-            _logger = logger;
+            _mediator = mediator;
         }
+
         [HttpGet("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceResponse>> GetStudent(int id)
         {
-            var response = new ServiceResponse();
-            try
-            {
-                var result = await _service.Get(p => p.Id == id);
-                if (result == null)
-                {
-                    return NotFound(null);
-                }
-                response.Data = result;
-                _logger.LogInformation("Get student from id");
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Success = false;
-                response.Message = ex.Message;
-                _logger.LogError("Throw an exception");
-                return BadRequest(response);
-            }
+            var query = new GetStudentQuery(id);
+            var response = await _mediator.Send(query);
+            if (response == null && response.Success == false) return BadRequest(response);
+            return Ok(response);
         }
         [HttpGet("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceResponse>> GetStudents()
         {
-            var response = new ServiceResponse();
-            try
-            {
-                var result = await _service.GetAll();
-                if (result == null)
-                {
-                    return NotFound(null);
-                }
-                response.Data = result;
-                _logger.LogInformation("Get students");
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Success = false;
-                response.Message = ex.Message;
-                _logger.LogError("Throw an exception");
-                return BadRequest(response);
-            }
+
+            var query = new GetStudentsQuery();
+            var response = await _mediator.Send(query);
+            if (response == null && response.Success == false) return BadRequest(response);
+            return Ok(response);
         }
         [HttpPost("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceResponse>> AddStudent([FromBody] StudentCreateDto studentCreateDto)
         {
-            var response = new ServiceResponse();
-            try
-            {
-                if (studentCreateDto == null)
-                {
-                    return NotFound(null);
-                }
-                var result = _mapper.Map<Student>(studentCreateDto);
-                await _service.Add(result);
-                _logger.LogInformation("Add student");
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Success = false;
-                response.Message = ex.Message;
-                _logger.LogError("Throw an exception");
-                return BadRequest(response);
-            }
+            var command = new AddStudentCommand(studentCreateDto);
+            var response = await _mediator.Send(command);
+            if (response == null && response.Success == false) return BadRequest(response);
+            return Ok(response);
         }
         [HttpDelete("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceResponse>> DeleteStudent(int id)
         {
-            var response = new ServiceResponse();
-            try
-            {
-                var result=await _service.Get(p=>p.Id== id);  
-
-                if (result == null)
-                {
-                    response.Message = "there is no existing data";
-                    return NotFound(response);
-                }
-                
-                await _service.Delete(result);
-                _logger.LogInformation("Delete student");
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Success = false;
-                response.Message = ex.Message;
-                _logger.LogError("Throw an exception");
-                return BadRequest(response);
-            }
+            var command = new DeleteStudentCommand(id);
+            var response = await _mediator.Send(command);
+            if (response == null && response.Success == false) return BadRequest(response);
+            return Ok(response);
         }
         [HttpPut("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ServiceResponse>> UpdateStudent(int id,[FromBody]StudentUpdateDto studentUpdateDto)
+        public async Task<ActionResult<ServiceResponse>> UpdateStudent(int id, [FromBody] StudentUpdateDto studentUpdateDto)
         {
-            var response = new ServiceResponse();
-            try
-            {
-                if (id != studentUpdateDto.Id||studentUpdateDto==null)
-                {
-                    response.Message = "updating is wrong";
-                    return NotFound(response);
-                }
-                var result = _mapper.Map<Student>(studentUpdateDto);
-
-                if (result == null)
-                {
-                    response.Message = "mapping is not happened";
-                    return NotFound(response);
-                }
-
-                await _service.Update(result);
-                _logger.LogInformation("Update student");
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                response.Success = false;
-                response.Message = ex.Message;
-                _logger.LogError("Throw an exception");
-                return BadRequest(response);
-            }
+            var command = new UpdateStudentCommand(id, studentUpdateDto);
+            var response = await _mediator.Send(command);
+            if (response == null && response.Success == false) return BadRequest(response);
+            return Ok(response);
         }
     }
 }
