c89e97c0d393795230e22c0946ecb2125cc71b9c
Murad Mammadzade <mammadzade03@gmail.com>
2023-01-11 14:46:27 +0400

    add query  and handler for get student from id

---
+++ CQRS_MEDIATR/Handlers/GetStudentHandler.cs
@@ -0,0 +1,40 @@
+﻿using CQRS_MEDIATR.Model;
+using CQRS_MEDIATR.Queries;
+using CQRS_MEDIATR.Services.Abstract.StudentService;
+
+
+namespace CQRS_MEDIATR.Handlers
+{
+    public class GetStudentHandler : IRequestHandler<GetStudentQuery, ServiceResponse>
+    {
+        private IStudentService _service;
+        private ILogger<Student> _logger;
+        public GetStudentHandler(IStudentService service, ILogger<Student> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+        public async Task<ServiceResponse> Handle(GetStudentQuery request, CancellationToken cancellationToken)
+        {
+            var response = new ServiceResponse();
+            try
+            {
+                var result = await _service.Get(p => p.Id == request.Id);
+                if (result == null)
+                {
+                    return null;
+                }
+                response.Data = result;
+                _logger.LogInformation("Get student from id");
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+                _logger.LogError("Throw an exception");
+                return response;
+            }
+        }
+    }
+}
---
+++ CQRS_MEDIATR/Queries/GetStudentQuery.cs
@@ -0,0 +1,14 @@
+﻿using CQRS_MEDIATR.Model;
+
+
+namespace CQRS_MEDIATR.Queries
+{
+    public class GetStudentQuery:IRequest<ServiceResponse>
+    {
+        public int Id { get; }
+        public GetStudentQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
